0be1953707558a846719c54448fb39d85e97f6ab
Moritz Meinhardt <moritzmeinhardt94@gmail.com>
2019-03-24 21:17:35 +0100

    next try

--- api/config/database.js
+++ api/config/database.js
@@ -1,7 +1,6 @@
 module.exports = {
-    database: 'mongodb://admin:28PaKCZITITv@localhost:27017/blog',
+    database: 'mongodb://localhost:27017/blog',
     //database: 'mongodb://localhost:27017/blog',
     secret: 'yoursecret', //TODO SECRET IN ENVIRONEMTVARIABLE,
-    username: 'admin',
-    password: '28PaKCZITITv'
+    useNewUrlParser: true
 }
