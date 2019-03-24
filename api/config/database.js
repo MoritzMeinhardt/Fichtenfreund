8b38e448709a97c75ab3656ecf16adda1a2261f5
Moritz Meinhardt <moritzmeinhardt94@gmail.com>
2019-03-24 21:02:36 +0100

    add database authentication

--- api/config/database.js
+++ api/config/database.js
@@ -1,4 +1,7 @@
 module.exports = {
-    database: 'mongodb://localhost:27017/blog',
-    secret: 'yoursecret' //TODO SECRET IN ENVIRONEMTVARIABLE
+    database: 'mongodb://admin:28PaKCZITITv@localhost:27017/blog',
+    //database: 'mongodb://localhost:27017/blog',
+    secret: 'yoursecret', //TODO SECRET IN ENVIRONEMTVARIABLE,
+    username: 'admin',
+    password: '28PaKCZITITv'
 }
