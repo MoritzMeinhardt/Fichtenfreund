e4bd8c9629ecf951e70575cdc908759c4f5dcd2b
Moritz Meinhardt <moritzmeinhardt94@gmail.com>
2019-03-24 20:42:45 +0100

    change databasename and return error code

--- api/config/database.js
+++ api/config/database.js
@@ -1,4 +1,4 @@
 module.exports = {
-    database: 'mongodb://localhost:27017/MyBlog',
+    database: 'mongodb://localhost:27017/blog',
     secret: 'yoursecret' //TODO SECRET IN ENVIRONEMTVARIABLE
 }
