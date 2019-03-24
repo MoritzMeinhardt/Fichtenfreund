d45f9128c1891f80fc7c4f92e241222f1050a8b7
Moritz Meinhardt <moritzmeinhardt94@gmail.com>
2019-03-24 19:39:21 +0100

    clean up

--- server.js
+++ server.js
@@ -1,10 +1,11 @@
 const express = require('express');
+const http = require('http'); // TEST
 const path = require('path');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const passport = require('passport');
 const mongoose = require('mongoose');
-const config = require('./config/database');
+const config = require('./api/config/database');
 
 // Connect to Database
 mongoose.connect(config.database);
@@ -20,11 +21,11 @@ mongoose.connection.on('error', (err) => {
 
 const app = express();
 
-const users = require('./routes/users');
-const blogs = require('./routes/blogs');
+const users = require('./api/routes/users');
+const blogs = require('./api/routes/blogs');
 
 // Port Number
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 // Body Parser Middleware
 app.use(bodyParser.json());
@@ -33,13 +34,13 @@ app.use(bodyParser.json());
 app.use(cors());
 
 // Set Static Folder
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'dist')));
 
 // Passport Middleware
 app.use(passport.initialize());
 app.use(passport.session());
 
-require('./config/passport')(passport);
+require('./api/config/passport')(passport);
 
 app.use('/api/users', users);
 app.use('/api/blogs', blogs);
