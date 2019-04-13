1f94973265aec34c31e1eb5328f25b51abfd89f0
Meinhardt, Moritz <Moritz.Meinhardt@EXXETA.com>
2019-04-13 20:44:14 +0200

    add instagram feature

--- src/webapp/src/app/footer/footer.component.ts
+++ src/webapp/src/app/footer/footer.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import {environment} from "../../environments/environment";
+import {HttpClient} from "@angular/common/http";
 
 @Component({
   selector: 'app-footer',
@@ -9,10 +10,21 @@ import {environment} from "../../environments/environment";
 export class FooterComponent implements OnInit {
 
   public basePath = environment.urlBase;
+  private accessToken = '12729826979.1677ed0.d922b0b63fd249de8f357ec44c27415d'; // TODO create one in case of error
+  private instagramApi = 'https://api.instagram.com/v1/users/self/media/recent/?access_token=';
+  public feed: string [] = new Array(10);
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
 
   ngOnInit() {
+    this.http.get(this.instagramApi + this.accessToken).subscribe(
+      (next: any) => {
+        for (let i = 0; i < this.feed.length; i++) {
+          this.feed[i] = next.data[i].images.standard_resolution.url;
+        }
+        },
+      (err) => {}
+    );
   }
 
 }
