7e78236ea4028ec789c57fbb436f589d38d67ce4
Meinhardt, Moritz <Moritz.Meinhardt@EXXETA.com>
2020-06-30 21:47:32 +0200

    fix typo

--- backend/src/main/java/de/fichtenfreund/backend/blog/BlogController.java
+++ backend/src/main/java/de/fichtenfreund/backend/blog/BlogController.java
@@ -6,7 +6,6 @@ import lombok.AllArgsConstructor;
 import org.modelmapper.ModelMapper;
 import org.springframework.web.bind.annotation.*;
 
-import javax.websocket.server.PathParam;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -40,7 +39,7 @@ public class BlogController {
     }
 
     @DeleteMapping("/{id}")
-    public void deleteBlog(@PathParam("id") Long id) {
+    public void deleteBlog(@PathVariable("id") Long id) {
         blogService.delete(id);
     }
 
